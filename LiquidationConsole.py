702a0ba78b15f0e4a9a9c1210b9460714cd1f0f9
Valentina <valenmesajaramillogmail.com>
2025-03-03 22:48:09 -0500

    Mejora de la consola

--- LiquidationConsole.py
+++ LiquidationConsole.py
@@ -2,14 +2,20 @@ import LiquidationLogic
 
 # Obtener los datos de entrada
 try:
-    salary = float(input("Ingrese el salario mensual del empleado: "))
-    days_worked = int(input("Ingrese el número de días trabajados: "))
+    salary = float(input("Ingrese el salario mensual: "))
+    days_worked = int(input("Ingrese los días trabajados: "))
     
-    # Realizar el proceso
+    # Validaciones
+    if salary <= 0 or days_worked <= 0:
+        raise ValueError("El salario y los días trabajados deben ser valores positivos.")
+    
+    # Realizar el cálculo
     liquidation = LiquidationLogic.calcLiquidation(salary, days_worked)
     
     # Mostrar los datos de salida
-    print(f"El valor de la liquidación es: {liquidation}")
+    print(f"La liquidación calculada es: {liquidation}")
+    
 except Exception as err:
     print(f"Error al calcular la liquidación: {err}")
+    
     
